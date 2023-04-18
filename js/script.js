3d031cae7de78ac8100af0f11d64316357a5f2fc
Ryan-Duffett <124389615+Ryan-Duffett@users.noreply.github.com>
2023-04-18 16:46:02 +0000

    Did all of the work

--- js/script.js
+++ js/script.js
@@ -1,9 +1,23 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
+// Created by: Ryan Duffet
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
 
-function buttonClicked() {
-alert("Hello, World!")
+'use strict'
+
+/**
+ * This function calculates area and perimeter of rectangle.
+ */
+function calculate () {
+  // input
+  const length = parseFloat(document.getElementById('length').value)
+  const width = parseFloat(document.getElementById('width').value)
+  const height = parseFloat(document.getElementById('height').value)
+
+  // process
+  const volume = (length * width * height) / 3
+
+  // output
+  document.getElementById('volume').innerHTML = 'Volume is: ' + volume + ' mm³'
 }
